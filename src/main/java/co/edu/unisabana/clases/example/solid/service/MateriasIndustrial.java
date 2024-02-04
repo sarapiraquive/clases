c0edac55a1c881dec5faded543036834e281f146
Sebas <sebastian.castellanos2012@gmail.com>
2024-02-04 11:15:23 -0500

    Interfaz para materiales agregada

--- src/main/java/co/edu/unisabana/clases/example/solid/service/MateriasIndustrial.java
+++ src/main/java/co/edu/unisabana/clases/example/solid/service/MateriasIndustrial.java
@@ -2,6 +2,6 @@ package co.edu.unisabana.clases.example.solid.service;
 
 public class MateriasIndustrial implements MateriasInterface{
     public String obtenerMaterias(String carrera) {
-        return "Procesos, Analitica de datos, Gestion de Calidad";
+        return "* Procesos, Analitica de datos, Gestion de Calidad";
     }
 }
